1ebc953f6acfacfb4a1268077ebed398a84367e7
Deonta Kilpatrick <deonta.kilpatrick@gmail.com>
2018-10-11 13:47:03 -0500

    Finished functionlity

--- client/client.js
+++ client/client.js
@@ -1,10 +1,18 @@
-console.log('Hello');
+console.log('Hello Deonta');
 
+// variables for all neccessary dom elements
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews');
+const API_URL = 'http://localhost:5000/mews';
+// end of variables
 
-loadingElement.style.display = 'none';
+// all executed cmds except adding the event listener
+loadingElement.style.display = '';
+listAllMews();
+// end of cmds
 
+// begin form event listener
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -18,5 +26,47 @@ form.addEventListener('submit', (event) => {
   console.log(mew);
   form.style.display = 'none';
   loadingElement.style.display = '';
-  
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: JSON.stringify(mew),
+    headers: {
+      'content-type': 'appliction/json'
+    }
+  }).then(response => response.json())
+    .then(createdMew => {
+      form.reset();
+      setTimeout(() => {
+        form.style.display = '';
+      }, 30000);
+      listAllMews();
+    });
 });
+// end of form event listener
+
+function listAllMews() {
+  mewsElement.innerHTML = '';
+  fetch(API_URL).then(response => response.json())
+    .then(mewz => {
+      mewz.reverse();
+      mewz.forEach(mew => {
+        const div = document.createElement('div');
+
+        const header = document.createElement('h3');
+        header.textContent = mew.name;
+
+        const contents = document.createElement('p');
+        contents.textContent = mew.content;
+
+        const date = document.createElement('small');
+        date.textContent = new Date(mew.created);
+
+        div.appendChild(header);
+        div.appendChild(contents);
+        div.appendChild(date);
+
+        mewsElement.appendChild(div);
+      });
+      loadingElement.style.display = 'none';
+    });
+}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const monk = require('monk'); // uses mongoDB
+const Filter = require('bad-words');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
+
+const db = monk('localhost/meower');
+const mews = db.get('mews');
+const filter = new Filter();
+
+app.use(cors());
+app.use(express.json());
+
+function isValidateMew (mew) {
+  return mew.name && mew.name.toString().trim() !== '' &&
+    mew.content && mew.content.toString().trim() !== '';
+}
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,8 +24,36 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/mews', (req, res) => {
+  mews.find().then(mew => {
+    res.json(mew);
+  });
+});
+
+app.use(rateLimit({
+  windowMs: 30 * 1000, // 30 seconds
+  max: 1 // limit each IP to 1 request per windowMs
+}));
+
 app.post('/mews', (req, res) => {
-  console.log(req.body);
+  if (isValidateMew(req.body)) {
+    // insert into db...
+    const mew = {
+      name: filter.clean(req.body.name.toString()),
+      content: filter.clean(req.body.content.toString()),
+      created: new Date()
+    };
+
+    mews.insert(mew).then(createdMew => {
+      res.json(createdMew);
+    });
+  } else {
+    // respond with error
+    res.status(422);
+    res.json({
+      message: 'Name and content required.'
+    });
+  }
 });
 
 app.listen(5000, () => {
